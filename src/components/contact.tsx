a9c60a78f8939afc7e5986ae5e26f4aac5d11cd1
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-08-26 18:53:14 +0200

    tooltips done

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,12 +2,12 @@ import LinkedIn from "@/components/icons/linkedin";
 import Mail from "@/components/icons/mail";
 import Phone from "@/components/icons/phone";
 import GitHub from "@/components/icons/github";
+import Download from "./icons/download";
 
 type SocialIcon = Record<string, string | any>;
 
 import { useLocale } from "next-intl";
 import { useTranslations } from "next-intl";
-import Download from "./icons/download";
 
 function Contact() {
   const t = useTranslations("basics");
@@ -24,7 +24,6 @@ function Contact() {
     en: "https://utfs.io/f/449fafa6-3002-499e-9413-cdb43d42c286-sfjao.pdf"
   };
 
-  console.log(locale);
   return (
     <>
       <ul className="flex gap-1">
@@ -32,7 +31,7 @@ function Contact() {
           <a
             className="p-2"
             href={`mailto:${t("email")}`}
-            title={`Enviar un correo electrónico a ${t("name")}`}
+            title={t("contact_tooltips.mail")}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -44,7 +43,7 @@ function Contact() {
           <a
             className="p-2"
             href={`tel:${t("phone")}`}
-            title={`Llamar a ${t("name")}`}
+            title={t("contact_tooltips.phone")}
           >
             <Phone />
           </a>
@@ -59,7 +58,7 @@ function Contact() {
             >
               <a
                 href={profile.url}
-                title={`Visitar el perfin de ${profile.username} en ${profile.network}`}
+                title={t(`contact_tooltips.${profile.network}`)}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="p-2"
@@ -72,7 +71,7 @@ function Contact() {
         <li className="flex cursor-pointer rounded-[0.25rem] border border-outline-color transition-colors hover:bg-zinc-600/40">
           <a
             href={portfolio_urls[locale]}
-            title="Descargar Curriculum"
+            title={t("contact_tooltips.download")}
             target="_blank"
             className="p-2"
             download
