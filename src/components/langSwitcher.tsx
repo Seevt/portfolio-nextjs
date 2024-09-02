a4625ac1ca85881eff0944180f1988248a5b7d73
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-09-02 12:49:35 +0200

    UX change for locales

--- src/components/langSwitcher.tsx
+++ src/components/langSwitcher.tsx
@@ -1,35 +1,37 @@
 "use client";
 
+import { usePathname, useRouter } from "@/navigation";
 import { locales, type Locale } from "@/config";
 
-import { usePathname, useRouter } from "@/navigation";
+import { useOptimistic, useTransition } from "react";
 
 export default function LocaleSwitcher({ locale }: { locale: Locale }) {
-  // `pathname` will contain the current
-  // route without the locale e.g. `/about`...
+  /* `pathname` will contain the current
+  route without the locale e.g. `/about`... */
+  // routing
   const pathname = usePathname();
   const router = useRouter();
+
+  // ux
+  const [isPending, startTransition] = useTransition();
+  const [optimisticLocale, setOptimisticLocale] = useOptimistic(locale);
 
   const changeLocale = (sentLocale: Locale) => {
     const newLocale = sentLocale as Locale;
 
-    // ...if the user chose Arabic ("ar-eg"),
-    // router.replace() will prefix the pathname
-    // with this `newLocale`, effectively changing
-    // languages by navigating to `/ar-eg/about`.
-    router.replace(pathname, { locale: newLocale });
+    setOptimisticLocale(newLocale);
+
+    startTransition(() => {
+      // ...if the user chose Arabic ("ar-eg"),
+      // router.replace() will prefix the pathname
+      // with this `newLocale`, effectively changing
+      // languages by navigating to `/ar-eg/about`.
+      router.replace(pathname, { locale: newLocale });
+    });
   };
 
   return (
     <div>
-      {/* <select value={locale} onChange={changeLocale} className="...">
-        {locales.map((loc) => (
-          <option key={loc} value={loc}>
-            {loc}
-          </option>
-        ))}
-      </select> */}
-
       <ul
         // gap must be minor than outer radius or viceversa
         style={
@@ -38,11 +40,11 @@ export default function LocaleSwitcher({ locale }: { locale: Locale }) {
             "--outer-radius": "0.375rem"
           } as React.CSSProperties
         }
-        className="border-outline-color fixed right-4 top-4 z-10 flex rounded-[--outer-radius] border p-[--gap] backdrop-blur-md max-md:[--gap:0.3rem_!important] max-md:[--outer-radius:0.1rem_!important]"
+        className="fixed right-4 top-4 z-10 flex rounded-[--outer-radius] border border-outline-color p-[--gap] backdrop-blur-md max-md:[--gap:0.3rem_!important] max-md:[--outer-radius:0.1rem_!important]"
       >
         {locales.map((loc) => (
           <li
-            className={`${locale === loc ? "rounded-[calc(var(--gap)_-_var(--outer-radius))] border border-zinc-700 bg-black-primary" : "border border-transparent"} text-md cursor-pointer px-4 py-2 font-light uppercase transition-all max-md:px-3 max-md:py-1`}
+            className={`${optimisticLocale === loc ? "rounded-[calc(var(--gap)_-_var(--outer-radius))] border border-zinc-700 bg-black-primary" : "border border-transparent"} ${isPending && optimisticLocale === loc ? "opacity-40" : ""} text-md cursor-pointer px-4 py-2 font-light uppercase transition-all max-md:px-3 max-md:py-1`}
             key={loc}
             onClick={() => changeLocale(loc)}
             value={loc}
@@ -54,5 +56,3 @@ export default function LocaleSwitcher({ locale }: { locale: Locale }) {
     </div>
   );
 }
-
-// after:[&:not(:last-child)]:mx-2 after:[&:not(:last-child)]:content-['|']
