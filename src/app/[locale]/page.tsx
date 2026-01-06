60949dc121dd5a82044ff55759d8c926208efa12
David <62206088+Seevt@users.noreply.github.com>
2026-01-06 19:21:39 +0100

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <Experience />
           </section>
           <section>
-            <Projects />
+            {/* <Projects /> */}
           </section>
           <section>
             <Education />
